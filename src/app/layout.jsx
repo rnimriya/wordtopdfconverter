80c45a17d893bb09b83107e289a41044f4d3d395
rakesh kumar <r.nimriya@gmail.com>
2026-07-05 13:17:18 +0530

    chore: add google site verification meta tag

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="0HIGRB4fKKzGFkLvQQK1CyYZC1125IsEjxOiGIk0onc" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
